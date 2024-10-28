b19d188bd8786a6779bcb44009c6c3e3bc1e763f
Michelle <michellemejiaparra4@gmail.com>
2024-10-28 11:29:08 -0500

    Add Link Reddit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,6 +5,7 @@ import SocialInsta from "@/app/public/assets/social-insta.svg";
 import SocialLinkedIn from "@/app/public/assets/social-linkedin.svg";
 import SocialYoutube from "@/app/public/assets/social-youtube.svg";
 import SocialPin from "@/app/public/assets/social-pin.svg";
+import SocialReddit from "@/app/public/assets/social-reddit.png";
 import Link from "next/link";
 
 export const Footer = () => {
@@ -21,11 +22,16 @@ export const Footer = () => {
             />
           </div>
         </div>
+        <a target="_blank" href="https://www.oasis-of-ideas.com/ideas/building-cooperative-networks-of-digital-transactions/">
+          <p className="mt-6 underline flex items-center justify-center">
+            <Image alt="" src={SocialReddit} className="mr-2 w-6 h-6" />
+            This is a functional Demo of the idea proposed
+          </p>
+        </a>
         <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
-                <Link href="#features">Features</Link>
-                <Link href="#about">About</Link>
-                {/* <Link href="#testimonials">Testimonials</Link> */}
-                <Link href="#contact">Contact Us</Link>
+          <Link href="#features">Features</Link>
+          <Link href="#about">About</Link>
+          <Link href="#contact">Contact Us</Link>
         </nav>
         <div className="flex justify-center gap-6 mt-6">
           <SocialInsta />
